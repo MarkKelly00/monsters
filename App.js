c94cc633fb6fbf004282a35e0e6944d33b70e5e3
Mako3 <big.mako@gmail.com>
2021-01-21 01:54:30 -0800

    finished the rolodex

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -10,6 +11,7 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
   }
 
   componentDidMount() {
@@ -18,7 +20,9 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }))
   }
 
-
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
 
   render() {
     const { monsters, searchField } = this.state;
@@ -27,10 +31,11 @@ class App extends Component {
       )
     return (
     <div className='App'>
-      <input 
-        type='search' 
-        placeholder='search monsters' 
-        onChange={e => this.setState({ searchField: e.target.value })} />
+      <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='search monsters' 
+          handleChange={this.handleChange}
+        />
       <CardList monsters={filteredMonsters} />
     </div>
     )
